24000b958a69de73fc5d18c2960a4e47d3c5bbf6
Ajay84sia <ajay9679339@gmail.com>
2023-08-03 22:03:32 +0530

    backend

---
+++ index.js
@@ -0,0 +1,37 @@
+const express = require('express');
+const axios = require('axios');
+require('dotenv').config();
+const cors = require('cors');
+
+const app = express();
+const port = process.env.PORT || 3000;
+
+app.use(express.json());
+app.use(cors())
+
+app.get('/joke', async (req, res) => {
+  try {
+    const keyword = req.query.keyword;
+    const response = await axios.post('https://api.openai.com/v1/engines/text-davinci-003/completions', {
+      prompt: `Joke about ${keyword}`,
+      max_tokens: 100,
+      temperature: 0.7,
+      n: 1
+    }, {
+      headers: {
+        'Authorization': `Bearer ${process.env.OPENAI_API_KEY}`,
+        'Content-Type': 'application/json'
+      }
+    });
+
+    const joke = response.data.choices[0].text.trim();
+    res.json({ joke });
+  } catch (error) {
+    console.error('Error:', error.response.data);
+    res.status(500).json({ error: 'Something went wrong' });
+  }
+});
+
+app.listen(port, () => {
+  console.log(`Server is running on port ${port}`);
+});
