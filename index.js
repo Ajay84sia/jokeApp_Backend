94aa5365d6a478777185bf78c80fd2eae0c8aef5
Ajay84sia <ajay9679339@gmail.com>
2023-08-03 22:12:05 +0530

    shayari route

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ require('dotenv').config();
 const cors = require('cors');
 
 const app = express();
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 8080;
 
 app.use(express.json());
 app.use(cors())
@@ -31,6 +31,30 @@ app.get('/joke', async (req, res) => {
     res.status(500).json({ error: 'Something went wrong' });
   }
 });
+
+app.get('/shayari', async (req, res) => {
+  try {
+    const keyword = req.query.keyword;
+    const response = await axios.post('https://api.openai.com/v1/engines/text-davinci-003/completions', {
+      prompt: `Shayari about ${keyword}`,
+      max_tokens: 100,
+      temperature: 0.7,
+      n: 1
+    }, {
+      headers: {
+        'Authorization': `Bearer ${process.env.OPENAI_API_KEY}`,
+        'Content-Type': 'application/json'
+      }
+    });
+
+    const shayari = response.data.choices[0].text.trim();
+    res.json({ shayari });
+  } catch (error) {
+    console.error('Error:', error.response.data);
+    res.status(500).json({ error: 'Something went wrong' });
+  }
+});
+
 
 app.listen(port, () => {
   console.log(`Server is running on port ${port}`);
